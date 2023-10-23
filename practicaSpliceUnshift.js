b1c9b303dc0770233876d9d663b440ef24b5e417
nocive.science <nocive.science@gmail.com>
2023-10-23 01:05:44 -0300

    quede con renderGameState()

--- practicaSpliceUnshift.js
+++ practicaSpliceUnshift.js
@@ -4,10 +4,11 @@ const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // console.log(lista.splice(1,1));
 // console.log(lista);
 
-// console.log(lista.unshift('hola', 'mundo'));
-// console.log(lista);
+console.log(lista.unshift('hola', 'mundo'));
+console.log(lista);
 
-// console.log(lista.shift('hola', 'mundo'));
-// console.log(lista);
-
- 
+// for (let i = 0; i < grid.length; i++) {
+//     const row = grid.splice(i, 1);
+//     row.unshift(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+//     console.log(row);
+// }
