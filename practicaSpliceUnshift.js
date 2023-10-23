cb1f63c0d25296a9bf7c2ce9d0f1e219227b2a80
nocive.science <nocive.science@gmail.com>
2023-10-22 22:16:49 -0300

    adding unshift

---
+++ practicaSpliceUnshift.js
@@ -0,0 +1,13 @@
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(lista.splice(3, 2));
+// console.log(lista); 
+// console.log(lista.splice(1,1));
+// console.log(lista);
+
+// console.log(lista.unshift('hola', 'mundo'));
+// console.log(lista);
+
+// console.log(lista.shift('hola', 'mundo'));
+// console.log(lista);
+
+ 
